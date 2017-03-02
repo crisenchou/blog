4eca4c889cae52a27d211fb33f6b770d03ca07ba
crisenchou <269298762@qq.com>
2017-03-02 22:28:11 +0800

    layouts

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,67 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <title>{{$title or ''}}</title>
-    <meta name="keywords" content="{{$keywords or ''}}"/>
-    <meta name="description" content="{{$description or ''}}"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <!-- basic styles -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}"/>
 
-
-    <!--[if IE 7]>
-    <link rel="stylesheet" href="{{asset('assets/css/font-awesome-ie7.min.css')}}"/>
-    <![endif]-->
-    <!-- page specific plugin styles -->
-    <!-- fonts -->
-    <!-- ace styles -->
-    <link rel="stylesheet" href="{{asset('assets/css/ace.min.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/ace-rtl.min.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/ace-skins.min.css')}}"/>
-    <!--[if lte IE 8]>
-    <link rel="stylesheet" href="{{asset('assets/css/ace-ie.min.css')}}"/>
-    <![endif]-->
-    <!-- inline styles related to this page -->
-
-    <!-- ace settings handler -->
-    <script src="{{asset('assets/js/ace-extra.min.js')}}"></script>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="{{asset('assets/js/html5shiv.js')}}"></script>
-    <script src="{{asset('assets/js/respond.min.js')}}"></script>
-    <![endif]-->
-
-    @yield('append_css')
-</head>
-<body>
 @include('layouts.header')
-<div class="main-container" id="main-container">
+<body class="no-skin">
+
+@include('layouts.nav')
+<div class="main-container ace-save-state" id="main-container">
     <script type="text/javascript">
-        try {
-            ace.settings.check('main-container', 'fixed')
-        } catch (e) {
-        }
+        try{ace.settings.loadState('main-container')}catch(e){}
     </script>
-    <div class="main-container-inner">
-        <a class="menu-toggler" id="menu-toggler" href="#">
-            <span class="menu-text"></span>
-        </a>
-
-        @include('layouts.menu')
-
-        @yield('content')
-
-    </div><!-- /.main-container-inner -->
 
     @include('layouts.sidebar')
 
+    <div class="main-content">
+        <div class="main-content-inner">
+          @include('layouts.breadcrumb')
+            <div class="page-content">
+                @include('layouts.settings')
+                <div class="page-header">
+                    <h1>
+                        Dashboard
+                        <small>
+                            <i class="ace-icon fa fa-angle-double-right"></i>
+                            overview &amp; stats
+                        </small>
+                    </h1>
+                </div><!-- /.page-header -->
+                @yield('content')
+
+            </div><!-- /.page-content -->
+        </div>
+    </div><!-- /.main-content -->
+
+    @include('layouts.footer')
+
     <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
-        <i class="icon-double-angle-up icon-only bigger-110"></i>
+        <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
     </a>
 </div><!-- /.main-container -->
-@include('layouts.footer')
+
+@include('layouts.script')
 </body>
 </html>
-
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,47 +1,43 @@
-<div class="ace-settings-container" id="ace-settings-container">
-    <div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
-        <i class="icon-cog bigger-150"></i>
+<div id="sidebar" class="sidebar                  responsive                    ace-save-state">
+    <script type="text/javascript">
+        try{ace.settings.loadState('sidebar')}catch(e){}
+    </script>
+
+    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+            <button class="btn btn-success">
+                <i class="ace-icon fa fa-signal"></i>
+            </button>
+
+            <button class="btn btn-info">
+                <i class="ace-icon fa fa-pencil"></i>
+            </button>
+
+            <button class="btn btn-warning">
+                <i class="ace-icon fa fa-users"></i>
+            </button>
+
+            <button class="btn btn-danger">
+                <i class="ace-icon fa fa-cogs"></i>
+            </button>
+        </div>
+
+        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+            <span class="btn btn-success"></span>
+
+            <span class="btn btn-info"></span>
+
+            <span class="btn btn-warning"></span>
+
+            <span class="btn btn-danger"></span>
+        </div>
+    </div><!-- /.sidebar-shortcuts -->
+
+    @include('layouts.menus')
+
+
+
+    <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
+        <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
     </div>
-
-    <div class="ace-settings-box" id="ace-settings-box">
-        <div>
-            <div class="pull-left">
-                <select id="skin-colorpicker" class="hide">
-                    <option data-skin="default" value="#438EB9">#438EB9</option>
-                    <option data-skin="skin-1" value="#222A2D">#222A2D</option>
-                    <option data-skin="skin-2" value="#C6487E">#C6487E</option>
-                    <option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
-                </select>
-            </div>
-            <span>&nbsp; 选择皮肤</span>
-        </div>
-
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-navbar" />
-            <label class="lbl" for="ace-settings-navbar"> 固定导航条</label>
-        </div>
-
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-sidebar" />
-            <label class="lbl" for="ace-settings-sidebar"> 固定滑动条</label>
-        </div>
-
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-breadcrumbs" />
-            <label class="lbl" for="ace-settings-breadcrumbs">固定面包屑</label>
-        </div>
-
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl" />
-            <label class="lbl" for="ace-settings-rtl">切换到左边</label>
-        </div>
-
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-add-container" />
-            <label class="lbl" for="ace-settings-add-container">
-                切换窄屏
-                <b></b>
-            </label>
-        </div>
-    </div>
-</div><!-- /#ace-settings-container -->
+</div>
