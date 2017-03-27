39a43eb406f048e44a40eddb5ff22306d1c5e1be
crisenchou <269298762@qq.com>
2017-03-27 11:55:41 +0000

    add footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,25 +1,11 @@
 <div class="footer">
     <div class="footer-inner">
         <div class="footer-content">
-						<span class="bigger-120">
-							<span class="blue bolder">Ace</span>
-							Application &copy; 2013-2014
-						</span>
-
-            &nbsp; &nbsp;
-            <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
-						</span>
+            <span class="bigger-120">
+                备案号:<span class="blue bolder"><a target="_blank" href="http://www.miitbeian.gov.cn">粤ICP备16043112号-1 </a></span>
+                crisenchou @ 2017
+            </span>
+            <span class="pull-right">人生从一开始就已经注定好了结局----crisenchou</span>
         </div>
     </div>
 </div>
